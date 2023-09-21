04fe4ad767e3367d56e369dfeb09648736eaeb4e
VictorPozdeev1 <114474092+VictorPozdeev1@users.noreply.github.com>
2023-09-22 01:58:20 +0400

    Temp/inline modules (#1) Инлайнит модули для обхода CORS-политик
    * Убирает type="module" для обхода CORS
    * Инлайнит модули прямо в index.js
    * Удаляет заинлайненный модуль
    * Удаляет заинлайненный модуль

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,97 @@
-import state from './todosStore.js';
-import { createView } from './todosContainerView.js';
+const state = {
+    loadFromStorage(fallback = {
+        _maxId: 2,
+        items: { 1: { text: 'Поспать' }, 2: { text: 'Покушать' } }
+    }) {
+        const data = JSON.parse(localStorage.getItem('todos')) ?? fallback;
+        this._maxId = Number(data._maxId);
+        this.items = new Map(Object.entries(data.items));
+    },
+
+    saveToStorage() {
+        const dataToSave = { _maxId: this._maxId, items: Object.fromEntries(this.items) };
+        localStorage.setItem('todos', JSON.stringify(dataToSave));
+    },
+
+    add(todoItem) {
+        this.items.set((++this._maxId).toString(), todoItem);
+        this.saveToStorage();
+        return this._maxId;
+    },
+
+    remove(todoId) {
+        this.items.delete(todoId);
+        this.saveToStorage();
+    },
+
+    setCompleteState(todoId, completed = true) {
+        this.items.set(todoId, {...this.items.get(todoId), completed});
+        this.saveToStorage();
+    },
+};
+
+state.loadFromStorage();
+
+const createView = (containerNodeId, completeHandler, deleteHandler) => ({
+    _container: document.getElementById(containerNodeId),
+
+    add(itemData, key) {
+        const domElementToAdd = document.createElement('li');
+        domElementToAdd.classList.add('todoItem');
+        domElementToAdd.textContent = itemData.text;
+
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = '\u2716';
+        deleteButton.classList.add('button', 'deleteButton', 'roundButton');
+        deleteButton.addEventListener('click', () => { deleteHandler(/* e.target.parentElement */ domElementToAdd) });
+        domElementToAdd.insertAdjacentElement('beforeend', deleteButton);
+
+        const completeButton = document.createElement('button');
+        completeButton.textContent = '\u2714';
+        completeButton.classList.add('button', 'completeButton', 'roundButton');
+        completeButton.addEventListener('click', () => { completeHandler(/* e.target.parentElement */ domElementToAdd) });
+        domElementToAdd.insertAdjacentElement('beforeend', completeButton);
+
+        domElementToAdd.dataset.id = key;
+        if (itemData.completed) {
+            domElementToAdd.dataset.completed = true;
+            domElementToAdd.classList.add('completed');
+        }
+        const whereTo = itemData.completed ? 'beforeend' : 'afterbegin';
+        this._container.insertAdjacentElement(whereTo, domElementToAdd);
+    },
+
+    setCompleteState(todoDomElement, newState = true) {
+        todoDomElement.dataset.completed = newState;
+        if (newState) {
+            todoDomElement.classList.add('completed');
+            this._container.append(todoDomElement);
+        } else {
+            todoDomElement.classList.remove('completed');
+            this._container.prepend(todoDomElement);
+        }
+    },
+
+    getFirst() {
+        return this._container.firstElementChild;
+    },
+
+    getNth(n) {
+        return this._container.children[n];
+    },
+
+    getLast() {
+        return this._container.lastElementChild;
+    },
+
+    markEveryNth(markAction, indexStart = 0, indexStep = 2) {
+        const allTodoDomElements = this._container.children;
+        for (let i = indexStart; i < allTodoDomElements.length; i += indexStep) {
+            const currentElement = allTodoDomElements[i];
+            markAction(currentElement);
+        }
+    }
+});
 
 (function init() {
     const newItem_innerContainer = document.getElementById('newItem_innerContainer');
